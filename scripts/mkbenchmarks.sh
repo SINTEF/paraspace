7b30af3ff6dd1f319b5f6690220b3d2276558743
Bjørnar Luteberget <bjornar.luteberget@sintef.no>
2023-01-31 09:46:55 +0100

    reorganized

--- scripts/mkbenchmarks.sh
+++ scripts/mkbenchmarks.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-python3 frontend/carbonaraproblem.py
-python3 frontend/ceramicproblem.py
-python3 frontend/goacproblem.py
+python3 src/frontend/carbonaraproblem.py
+python3 src/frontend/ceramicproblem.py
+python3 src/frontend/goacproblem.py
